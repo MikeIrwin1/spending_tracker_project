8f2be74cf84c14d22d457544c2e3de553e8126d0
Michael Irwin <mike.irwin1@gmail.com>
2018-12-16 14:38:06 +0000

    merchant controller and erb completed

--- controllers/merchant-controller.rb
+++ controllers/merchant-controller.rb
@@ -3,6 +3,41 @@ require('sinatra/contrib/all')
 require_relative('../models/merchant')
 also_reload('../models/*')
 
+# display
+
 get '/merchants' do
+  @merchants = Merchant.all()
   erb(:"merchant/index")
+end
+
+# add new merchant
+
+get '/merchants/new' do
+  erb(:"merchant/new")
+end
+
+post '/merchants' do
+  @merchant = Merchant.new( params )
+  @merchant.save()
+  redirect to '/merchants'
+end
+
+# delete merchant
+
+post '/merchants/:id/delete' do
+  Merchant.delete(params[:id])
+  redirect to '/merchants'
+end
+
+# edit
+
+get '/merchants/:id/edit' do
+  @merchant = Merchant.find(params[:id])
+  erb(:"merchant/edit")
+end
+
+post '/merchants/:id/edit' do
+  merchant = Merchant.new(params)
+  merchant.update
+  redirect to '/merchants'
 end
--- models/merchant.rb
+++ models/merchant.rb
@@ -25,9 +25,9 @@ class Merchant
     SqlRunner.run(sql)
   end
 
-  def delete()
+  def self.delete(id)
     sql = "DELETE FROM merchants WHERE id = $1"
-    values = [@id]
+    values = [id]
     SqlRunner.run(sql, values)
   end
 
